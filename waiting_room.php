dad2e9a2e5f9272d5919b6c1af48489ea0adbdb2
= <=>
2024-11-12 10:26:17 +0100

    infinite refresh

--- waiting_room.php
+++ waiting_room.php
@@ -47,6 +47,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
         crossorigin="anonymous" />
+
+    <meta http-equiv="refresh" content="1">
 </head>
 
 <body>
